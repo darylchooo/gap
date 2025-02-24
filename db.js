cfb96b6dddbea3a80575d2e74a19a6b0c0d276a2
Daryl Choo <158234573+darylchooo@users.noreply.github.com>
2025-02-23 19:35:23 -0500

    First Update

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config(); // Load environment variables from a .env file
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: localhost,
+    user: root,
+    password: @*Dec2001,
+    database: gap,
 });
 
 connection.connect((err) => {
